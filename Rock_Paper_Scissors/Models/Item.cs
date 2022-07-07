ea59bb23466960bfd7d21825baf642e28b46645e
Jimmy-Phasha <Jimmy-Phasha@users.noreply.github.com>
2022-07-07 02:21:49 +0200

    Skeleton MVC for the program
    mostly the skeleton structure of a basic MVC web app has been done with 2 methods in the home controller to help the computer have its choices

---
+++ Rock_Paper_Scissors/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
